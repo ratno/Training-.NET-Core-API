574b9a05d8cd15be7ad2f8d52cff39ac28be52cf
Ratno Putro Sulistiyono <ratno@knoqdown.com>
2017-07-22 19:52:41 +0700

    add model pegawai

---
+++ Pegawai/Models/Pegawai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pegawai.Models
+{
+    public class Pegawai
+    {
+        public long id { get; set; }
+        public string nama { get; set; }
+        public string gender { get; set; }
+        public string hp { get; set; }
+    }
+}
